4b39c51fbb7947a45e882e59f50b8eef7e060712
Juan David Massanet Puentes <davidmassanet@gmail.com>
2022-10-06 12:01:30 +0200

    Reinicio de funcionalidad y mejora sustancial en el funcionamiento de la misma

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,37 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Game.css';
 
 
 function Game() {
-  
-    let rondaActual = 0; // Entre ronda y ronda poner un tiempo y una pantalla en ¿blanco? indicando puntuación y ronda actual. usar z-index
-    let puntuacion = 0;
-    let personajesElegidos = [];
-    let personajeGanador;
-    let personajesGanadoresAnteriores = [];
+    const [cargado, setCargado] = useState(false);
+    // Entre ronda y ronda poner un tiempo y una pantalla en ¿blanco? indicando puntuación y ronda actual. usar z-index
+    const [personajesElegidos, setPersonajesElegidos] = useState([]);
+    const [rondaActual, setRondaActual] = useState(1);
+    const [puntuacion, setPuntuacion] = useState(0);
+    const [personajeGanador, setPersonajeGanador] = useState([1]);
+    const [personajesGanadoresAnteriores, setPersonajesGanadoresAnteriores] = useState([]);
+    let personajesGanadoresAnterioresAux = [];
 
     // http://drive.google.com/uc?export=view&id=FILEID -- recibir desde google drive
-    const personajes = [{name: "Padmé Amidala", img: "http://drive.google.com/uc?export=view&id=1VdR8n1HB8Cc5e0NKAKNEpL6lL-hAATGG"}, {name: "Darth Maul", img: "http://drive.google.com/uc?export=view&id=1uejA6sbDN8xE95T2DmZjEVeAXoBVLZRZ"}, {name: "Obi-Wan Kenobi", img: "http://drive.google.com/uc?export=view&id=1cF4dpEG3qf1iRdUnPKMTSLT2TWVBV9hA"}, 
-                        {name: "Anakin SkyWalker", img: "http://drive.google.com/uc?export=view&id=1VIbrx9__kGxTDx4sOIqLO7Y0qflaDx-Z"}, {name: "Qui-Gon Jinn", img: "http://drive.google.com/uc?export=view&id=1rUSd_Fz9akU0lCuKcIXEcNYUaJABCZ2R"}, {name: "Jar Jar Binks", img: "http://drive.google.com/uc?export=view&id=1A8q599C3pHtpc-ma4OpD1uzveutJ7zEE"}, 
-                        {name: "Yoda", img: "http://drive.google.com/uc?export=view&id=14EccBQMak3IV685eObGC0SSSe5L5mpP6"}, {name: "Mace Windu", img: "http://drive.google.com/uc?export=view&id=17k4r_S0wCCgCquaiMWY_dTW6TPhmjQnY"}, {name: "C3PO", img: "http://drive.google.com/uc?export=view&id=1VyWKxj3GqyIWQWdU42wCgNyVNiROfdr3"}, 
-                        {name: "R2D2", img: "http://drive.google.com/uc?export=view&id=1HydW-NwStvlArUThoY4iHWRKRN7n8JoP"}, {name: "Jango Fett", img: "http://drive.google.com/uc?export=view&id=1WrZZWVMtqizquNnyV4kmRLecx6sLq_e7"}, {name: "Boba Fett", img: "http://drive.google.com/uc?export=view&id=1Ilr2uQyoRAsz7N8DmW5Ja5wT2wkxS8CC"}, 
-                        {name: "Taun We", img: "http://drive.google.com/uc?export=view&id=1tEv6BaqlOA7Sn97JQZQzU_JFVw7Wljwm"}, {name: "Clon", img: "http://drive.google.com/uc?export=view&id=1uMU1Rj8hchNdWJGj1hIixAA8IvHw3Kpc"}, {name: "Conde Dooku", img: "http://drive.google.com/uc?export=view&id=1nE0ybgyEP8ejBmtKvE5Wm87tmd4k3On1"}, 
-                        {name: "Grievous", img: "http://drive.google.com/uc?export=view&id=1ZHXAyKH8XdkKNrigYQgjaX9nsmPi_iVM"}, {name: "Luke SkyWalker", img: "http://drive.google.com/uc?export=view&id=1zQ1wEKR2Knyi_YIiZ3UE9k1dUTxtJEXt"}, {name: "Han Solo", img: "http://drive.google.com/uc?export=view&id=1K-SgmnJoIYrW17gVxU5vbv2loaQnAxKK"}, 
-                        {name: "Leia Organa", img: "http://drive.google.com/uc?export=view&id=15lRHIRz_njWTV1qAR9kXi5I18jroyIIo"}, {name: "Chewbacca", img: "http://drive.google.com/uc?export=view&id=1qBjbOmbEw9n3-BVaDJuH1X_YqoyqZ0Kb"}, {name: "Jabba el Hutt", img: "http://drive.google.com/uc?export=view&id=1GLT1p6RnCv8RaMYZVjYNL83wXi6WKJYy"}, 
-                        {name: "Jawa", img: "http://drive.google.com/uc?export=view&id=1VxEevzZVmanxoqRZA1VvZAKjndLzvP74"}, {name: "Bantha", img: "http://drive.google.com/uc?export=view&id=1uuRuofFE6tAyOWTPRdD6O12obpU3591j"}, {name: "Tusken Raider", img: "http://drive.google.com/uc?export=view&id=1bDGwCLDguePdhLMUQfywvIUa5ftv9WI-"}, 
-                        {name: "StormTrooper", img: "http://drive.google.com/uc?export=view&id=1KpxBd_kwE0kto7fOzqrdM5ahxZET4Eyw"}, {name: "Darth Vader", img: "http://drive.google.com/uc?export=view&id=11s-HPxsoJOdFZgyg9xcT3F_R6Pf2Z9FC"}, {name: "Palpatine", img: "http://drive.google.com/uc?export=view&id=1Hdn5yZrMRJTJLd8jju3j47P9qduXKW_z"}, 
-                        {name: "Lando Calrissian", img: "http://drive.google.com/uc?export=view&id=1anVrbD9BqDE5A1lN3ngKD1UCMlqX1RkD"}, {name: "SnowTrooper", img: "http://drive.google.com/uc?export=view&id=1OzOoTMpEAQw7Gz5LJxbiJjjE_LGQrmIy"}, {name: "Kylo Ren", img: "http://drive.google.com/uc?export=view&id=15JWx9VUr23grneG0l1NLqWS0Pqx5efIl"}, 
-                        {name: "Rey", img: "http://drive.google.com/uc?export=view&id=17NubhdPFVBnSrwydhRUN2CEIb7Vz_Zfq"}, {name: "Poe Dameron", img: "http://drive.google.com/uc?export=view&id=1SOMs0pgTZjNnU_I3g22WjPRuGpv7oBIu"}, {name: "BB-8", img: "http://drive.google.com/uc?export=view&id=11Qxs5GCJB3WGJrEeoWBW0_HV_CRwO0WZ"}, 
-                        {name: "Finn", img: "http://drive.google.com/uc?export=view&id=1qZlNMgMhu8ru8nQmxxtSxYhm4g0j3wc4"}, {name: "Phasma", img: "http://drive.google.com/uc?export=view&id=1ssjhru0DSu9U2spxcG1IGAp_tGe-A0xu"}, {name: "AckBar", img: "http://drive.google.com/uc?export=view&id=1XxklqK2MHm3GXoQOJ0ggZetSMK_CMPZW"}, 
-                        {name: "Rose Tico", img: "http://drive.google.com/uc?export=view&id=1kmu8AhZvFySVCgU39wYyv3wLA1VUyxAe"}, {name: "First Order Stormtrooper", img: "http://drive.google.com/uc?export=view&id=1_-luWiOHbuuDa0jVQCLjg2ue_oOAfsaQ"}, {name: "General Hux", img: "http://drive.google.com/uc?export=view&id=1xJvFFGyT_qLtqBwjuSoYsZZAm_kGpUg_"}, 
-                        {name: "Maz Kanata", img: "http://drive.google.com/uc?export=view&id=1o6xia7WPWyALFWdi_f204GrO0k9IPiE8"}]
-    function elegirPersonajes() {
+    const personajes = [{name: "padmé amidala", img: "http://drive.google.com/uc?export=view&id=1VdR8n1HB8Cc5e0NKAKNEpL6lL-hAATGG"}, {name: "darth maul", img: "http://drive.google.com/uc?export=view&id=1uejA6sbDN8xE95T2DmZjEVeAXoBVLZRZ"}, {name: "obi-wan kenobi", img: "http://drive.google.com/uc?export=view&id=1cF4dpEG3qf1iRdUnPKMTSLT2TWVBV9hA"}, 
+                        {name: "anakin skyWalker", img: "http://drive.google.com/uc?export=view&id=1VIbrx9__kGxTDx4sOIqLO7Y0qflaDx-Z"}, {name: "qui-gon jinn", img: "http://drive.google.com/uc?export=view&id=1rUSd_Fz9akU0lCuKcIXEcNYUaJABCZ2R"}, {name: "jar jar binks", img: "http://drive.google.com/uc?export=view&id=1A8q599C3pHtpc-ma4OpD1uzveutJ7zEE"}, 
+                        {name: "yoda", img: "http://drive.google.com/uc?export=view&id=14EccBQMak3IV685eObGC0SSSe5L5mpP6"}, {name: "mace windu", img: "http://drive.google.com/uc?export=view&id=17k4r_S0wCCgCquaiMWY_dTW6TPhmjQnY"}, {name: "c3po", img: "http://drive.google.com/uc?export=view&id=1VyWKxj3GqyIWQWdU42wCgNyVNiROfdr3"}, 
+                        {name: "r2d2", img: "http://drive.google.com/uc?export=view&id=1HydW-NwStvlArUThoY4iHWRKRN7n8JoP"}, {name: "jango fett", img: "http://drive.google.com/uc?export=view&id=1WrZZWVMtqizquNnyV4kmRLecx6sLq_e7"}, {name: "boba fett", img: "http://drive.google.com/uc?export=view&id=1Ilr2uQyoRAsz7N8DmW5Ja5wT2wkxS8CC"}, 
+                        {name: "taun we", img: "http://drive.google.com/uc?export=view&id=1tEv6BaqlOA7Sn97JQZQzU_JFVw7Wljwm"}, {name: "clon", img: "http://drive.google.com/uc?export=view&id=1uMU1Rj8hchNdWJGj1hIixAA8IvHw3Kpc"}, {name: "conde dooku", img: "http://drive.google.com/uc?export=view&id=1nE0ybgyEP8ejBmtKvE5Wm87tmd4k3On1"}, 
+                        {name: "grievous", img: "http://drive.google.com/uc?export=view&id=1ZHXAyKH8XdkKNrigYQgjaX9nsmPi_iVM"}, {name: "luke skyWalker", img: "http://drive.google.com/uc?export=view&id=1zQ1wEKR2Knyi_YIiZ3UE9k1dUTxtJEXt"}, {name: "han solo", img: "http://drive.google.com/uc?export=view&id=1K-SgmnJoIYrW17gVxU5vbv2loaQnAxKK"}, 
+                        {name: "leia organa", img: "http://drive.google.com/uc?export=view&id=15lRHIRz_njWTV1qAR9kXi5I18jroyIIo"}, {name: "chewbacca", img: "http://drive.google.com/uc?export=view&id=1qBjbOmbEw9n3-BVaDJuH1X_YqoyqZ0Kb"}, {name: "jabba el hutt", img: "http://drive.google.com/uc?export=view&id=1GLT1p6RnCv8RaMYZVjYNL83wXi6WKJYy"}, 
+                        {name: "jawa", img: "http://drive.google.com/uc?export=view&id=1VxEevzZVmanxoqRZA1VvZAKjndLzvP74"}, {name: "bantha", img: "http://drive.google.com/uc?export=view&id=1uuRuofFE6tAyOWTPRdD6O12obpU3591j"}, {name: "tusken raider", img: "http://drive.google.com/uc?export=view&id=1bDGwCLDguePdhLMUQfywvIUa5ftv9WI-"}, 
+                        {name: "stormtrooper", img: "http://drive.google.com/uc?export=view&id=1KpxBd_kwE0kto7fOzqrdM5ahxZET4Eyw"}, {name: "darth vader", img: "http://drive.google.com/uc?export=view&id=11s-HPxsoJOdFZgyg9xcT3F_R6Pf2Z9FC"}, {name: "palpatine", img: "http://drive.google.com/uc?export=view&id=1Hdn5yZrMRJTJLd8jju3j47P9qduXKW_z"}, 
+                        {name: "lando calrissian", img: "http://drive.google.com/uc?export=view&id=1anVrbD9BqDE5A1lN3ngKD1UCMlqX1RkD"}, {name: "snowtrooper", img: "http://drive.google.com/uc?export=view&id=1OzOoTMpEAQw7Gz5LJxbiJjjE_LGQrmIy"}, {name: "kylo ren", img: "http://drive.google.com/uc?export=view&id=15JWx9VUr23grneG0l1NLqWS0Pqx5efIl"}, 
+                        {name: "rey", img: "http://drive.google.com/uc?export=view&id=17NubhdPFVBnSrwydhRUN2CEIb7Vz_Zfq"}, {name: "poe dameron", img: "http://drive.google.com/uc?export=view&id=1SOMs0pgTZjNnU_I3g22WjPRuGpv7oBIu"}, {name: "bb-8", img: "http://drive.google.com/uc?export=view&id=11Qxs5GCJB3WGJrEeoWBW0_HV_CRwO0WZ"}, 
+                        {name: "finn", img: "http://drive.google.com/uc?export=view&id=1qZlNMgMhu8ru8nQmxxtSxYhm4g0j3wc4"}, {name: "phasma", img: "http://drive.google.com/uc?export=view&id=1ssjhru0DSu9U2spxcG1IGAp_tGe-A0xu"}, {name: "ackBar", img: "http://drive.google.com/uc?export=view&id=1XxklqK2MHm3GXoQOJ0ggZetSMK_CMPZW"}, 
+                        {name: "rose tico", img: "http://drive.google.com/uc?export=view&id=1kmu8AhZvFySVCgU39wYyv3wLA1VUyxAe"}, {name: "first order stormtrooper", img: "http://drive.google.com/uc?export=view&id=1_-luWiOHbuuDa0jVQCLjg2ue_oOAfsaQ"}, {name: "general hux", img: "http://drive.google.com/uc?export=view&id=1xJvFFGyT_qLtqBwjuSoYsZZAm_kGpUg_"}, 
+                        {name: "maz kanata", img: "http://drive.google.com/uc?export=view&id=1o6xia7WPWyALFWdi_f204GrO0k9IPiE8"}]
+    
+    function obtenerPersonajes() {
         
-        while(personajesElegidos.length !== 4){
-            let personajeElegido = Math.floor(Math.random()*40);
+        let personajesElegidosAux = [];
+        let contador = 0;
+        while(contador !== 4){
+            let personajeElegido = Math.floor(Math.random()*personajes.length);
             let personajeUsado = false;
-            for (let index = 0; index < personajesElegidos.length; index++) {
-                if (personajeElegido == personajesElegidos[index]) {
+            
+            console.log(personajesElegidos);
+            console.log(personajeElegido);
+            for (let index = 0; index < personajesElegidosAux.length; index++) {
+                if (personajeElegido == personajesElegidosAux[index]) {
                     personajeUsado = true;
                 }
             }
@@ -40,31 +48,83 @@ function Game() {
                     personajeUsado = true;
                 }
             }
+            for (let index = 0; index < personajesGanadoresAnterioresAux.length; index++) {
+                if (personajeElegido == personajesGanadoresAnterioresAux[index]) {
+                    personajeUsado = true;
+                }
+            }
             if (!personajeUsado){
-                personajesElegidos.push(personajeElegido);
+                contador++;
+                personajesElegidosAux.push(personajeElegido);
             }
         }
-        elegirPersonajeGanador();
+        setPersonajesElegidos(personajesElegidosAux);
+        setCargado(true);
+        obtenerGanador(personajesElegidosAux);
     }
 
-    function elegirPersonajeGanador() {
+    function obtenerGanador(personajesElegidos) {
         let posicionPersonaje = Math.floor(Math.random()*4);
-        personajeGanador = personajesElegidos[posicionPersonaje];
-        personajesGanadoresAnteriores.push(personajeGanador);
-    }
-
-    function pasarSiguienteRonda() {
-        rondaActual++;
+        setPersonajeGanador(personajesElegidos[posicionPersonaje]);
+        setPersonajesGanadoresAnteriores(personajesGanadoresAnteriores => [...personajesGanadoresAnteriores, personajesElegidos[posicionPersonaje]])
+        personajesGanadoresAnterioresAux.push(personajesElegidos[posicionPersonaje]);
     }
     
-    useEffect(pasarSiguienteRonda, []);
-    useEffect(elegirPersonajes, [rondaActual]);
+    function obtenerTarjetasPersonajes() {
+        let contador = 0;
+        let tarjetaPersonaje;
+    
+        let tarjetasPersonajes = [];
+        
+        do{
+            tarjetaPersonaje = personajes[personajesElegidos[contador]];
+            tarjetasPersonajes.push(tarjetaPersonaje);
+            contador++;
+        }while (4 > tarjetasPersonajes.length);
+        return tarjetasPersonajes.map(muestraTarjeta);
+    }
+
+    function esEleccionCorrecta(event) {
+        let ganador = document.getElementById("ganador");
+        if (rondaActual == 10) {
+            // nueva pantalla diciendo puntuacion
+        }
+        setRondaActual(rondaActual + 1);
+
+        if(ganador.textContent == event.target.name){
+            setPuntuacion(puntuacion + 1)
+            // animacion de acierto
+        }else{
+            // animacion de fallo
+        }
+
+        obtenerPersonajes();
+    }
+    
+    function muestraTarjeta(personaje, indice) {
+        return (
+            <div key={indice} className="col-sm-3 d-flex flex-row justify-content-around"><img onClick={esEleccionCorrecta} className='w-75 h-75 eleccion' name={personaje.name} src={personaje.img}/></div>
+        );
+    }
+
+    useEffect(obtenerPersonajes, []);
 
     return (
         <div id="game">
-            <div className='star-wars-font'>
-                {(personajesElegidos.length !== 4 ? "" : "Hay " + personajesElegidos.length + " personajes y el personaje ganador es " + personajes[personajeGanador].name )}
+            <div className='row'>
+                <div className='col-sm-6 ronda bg-info'>
+                    Ronda {rondaActual} de 10
+                </div>
+                <div className='col-sm-6 puntuacion bg-primary'>
+                    Puntuacion: {puntuacion} de 10
+                </div>
             </div>
+            <div className='tarjetas row'>
+                {cargado ? obtenerTarjetasPersonajes() : ""}
+            </div>
+            <h1 id='ganador' className='star-wars-font-secundary bg-secondary d-inline px-4 pb-2 rounded'>{personajes[personajeGanador].name}</h1>
+            
+            {/*  */}
         </div>
     );
 }
